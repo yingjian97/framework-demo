99a2e00b0dbf00ea1f943afb4b96eb7b249df5a2
应坚 <2551314524@qq.com>
2025-12-01 01:51:02 +0800

    Datasource

---
+++ datasource-demo/src/main/java/site/yingjian/aop/demo/DatasourceTest.java
@@ -0,0 +1,12 @@
+package site.yingjian.aop.demo;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class DatasourceTest {
+
+    public static void main(String[] args) {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+
+    }
+}
