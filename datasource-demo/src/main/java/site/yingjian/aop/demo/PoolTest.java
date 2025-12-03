b81a4ab6d73d9441103aa6adfdbcd88b301f9a54
应坚 <2551314524@qq.com>
2025-12-03 21:41:06 +0800

    Pool

---
+++ datasource-demo/src/main/java/site/yingjian/aop/demo/PoolTest.java
@@ -0,0 +1,51 @@
+package site.yingjian.aop.demo;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import java.beans.PropertyVetoException;
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class PoolTest {
+
+    public static void main(String[] args) throws PropertyVetoException, SQLException {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+        dataSource.setUser("root");
+        dataSource.setPassword("19970918lkdys_A");
+        dataSource.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/test_db");
+        dataSource.setInitialPoolSize(1);
+        dataSource.setMaxPoolSize(1);
+//        Connection connection = dataSource.getConnection();
+//        System.out.println(connection.getClass().getName());
+//        Object inner = getInner(connection);
+//        System.out.println(inner.getClass().getName());
+        Connection connection1 = dataSource.getConnection();
+        Object inner1 = getInner(connection1);
+        connection1.close();
+        Connection connection2 = dataSource.getConnection();
+        Object inner2 = getInner(connection2);
+        System.out.println(connection1 == connection2);
+        System.out.println(inner1.getClass().getName());
+        System.out.println(inner2.getClass().getName());
+        System.out.println(inner1 == inner2);
+
+
+    }
+
+    public static Object getInner(Object connection) {
+        Object result = null;
+        Field field = null;
+        try {
+            field = connection.getClass().getDeclaredField("inner");
+            field.setAccessible(true);
+            result = field.get(connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
+}
